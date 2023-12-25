b884898c788c77eea7131543c4b2c9c7a59d3e67
Mohamed Amine Ben Safta <mohamedaminebensafta@gmail.com>
2023-12-25 23:36:43 +0100

    End to end tests are done

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <>
-            <footer className="text-center  bg-info text-light fixed-bottom">
+            <footer className="text-center  bg-info text-light fixed-bottom" data-testid="footer-component">
                 <p className="p-2"><strong>Created with love <span className='text-danger' title='heart'>&#9829;</span></strong></p>
             </footer>
         </>
--- src/Search.jsx
+++ src/Search.jsx
@@ -4,8 +4,8 @@ function Search({ word, handleSearch, doSearch }) {
             <div className="row mt-3">
                 <div className="col-4"></div>
                 <div className="col-4">
-                    <input type="text" name="word" className="form-control" value={word} placeholder="Word" id="word" onChange={handleSearch} />
-                    <button className="btn btn-success mt-2" type="button" title="Search button" name="Search-button" onClick={doSearch}>Search</button>
+                    <input type="text" name="word" className="form-control" value={word} placeholder="Word" id="word" onChange={handleSearch} data-testid="search-input" />
+                    <button className="btn btn-success mt-2" type="button" title="Search button" name="Search-button" data-testid="search-button" onClick={doSearch}>Search</button>
                 </div>
                 <div className="col-4"></div>
             </div>
