773831458c7ca3b8064d9f8c9888ee9e697026dd
Mohamed Amine Ben Safta <mohamedaminebensafta@gmail.com>
2023-12-25 16:16:52 +0100

    Dev is completed

---
+++ src/Item.jsx
@@ -0,0 +1,12 @@
+function Item({ item, index }) {
+    return (
+        <>
+            <tr>
+                <th scope="row">{index + 1}</th>
+                <td className="fw-bolder">{item.partOfSpeech}</td>
+                <td className="fst-italic"> {item.definition.definition}  </td>
+            </tr>
+        </>
+    );
+}
+export default Item;
